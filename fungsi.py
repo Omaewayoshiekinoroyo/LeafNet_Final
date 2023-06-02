ef3859d06f3637e013dd115553c7126da26b95d8
Omaewayoshiekinoroyo <131304986+Omaewayoshiekinoroyo@users.noreply.github.com>
2023-06-02 17:14:07 +0700

    fungsi.py

--- fungsi.py
+++ fungsi.py
@@ -5,27 +5,6 @@ from tensorflow.keras.applications import mobilenet
 from tensorflow.keras.layers import GlobalAveragePooling2D
 
 def make_model():
-    model = Sequential()
-    model.add(Conv2D(16, (3, 3), input_shape=(32, 32, 3), padding='same'))
-    model.add(LeakyReLU(0.1))
-    model.add(Conv2D(32, (3, 3), padding='same'))
-    model.add(LeakyReLU(0.1))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Conv2D(32, (3, 3), padding='same'))
-    model.add(LeakyReLU(0.1))
-    model.add(Conv2D(64, (3, 3), padding='same'))
-    model.add(LeakyReLU(0.1))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(256))
-    model.add(LeakyReLU(0.1))
-    model.add(Dropout(0.5))
-    model.add(Dense(10, activation='softmax'))
-
-    return model
-
 mobilenet = mobilenet.MobileNet(input_shape=(224, 224, 3),
                                 include_top=False,
                                 weights='imagenet')
@@ -37,3 +16,6 @@ model.add(Flatten())
 model.add(Dense(1024, activation="relu"))
 model.add(Dense(512, activation="relu"))
 model.add(Dense(4, activation="softmax", name="classification"))
+
+
+    return model
