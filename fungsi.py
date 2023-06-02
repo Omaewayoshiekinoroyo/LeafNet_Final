48e0987954959f3d6518fc440ff6bc53b4a9af8f
Omaewayoshiekinoroyo <131304986+Omaewayoshiekinoroyo@users.noreply.github.com>
2023-06-02 16:20:02 +0700

    fungsi.py

--- fungsi.py
+++ fungsi.py
@@ -1,28 +1,25 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import GlobalAveragePooling2D, Flatten, Dense
-from tensorflow.keras.applications.mobilenet import MobileNet
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, Dropout,LeakyReLU
 
-def create_model():
-    mobilenet = MobileNet(input_shape=(224, 224, 3),
-                          include_top=False,
-                          weights='imagenet')
-
+def make_model():
     model = Sequential()
-    model.add(mobilenet)
-    model.add(GlobalAveragePooling2D())
+    model.add(Conv2D(16, (3, 3), input_shape=(32, 32, 3), padding='same'))
+    model.add(LeakyReLU(0.1))
+    model.add(Conv2D(32, (3, 3), padding='same'))
+    model.add(LeakyReLU(0.1))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Conv2D(32, (3, 3), padding='same'))
+    model.add(LeakyReLU(0.1))
+    model.add(Conv2D(64, (3, 3), padding='same'))
+    model.add(LeakyReLU(0.1))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
     model.add(Flatten())
-    model.add(Dense(1024, activation="relu"))
-    model.add(Dense(512, activation="relu"))
-    model.add(Dense(4, activation="softmax", name="classification"))
-
-    optimizer = tf.keras.optimizers.SGD(learning_rate=0.0005, momentum=0.9)
-    model.compile(optimizer=optimizer,
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-
+    model.add(Dense(256))
+    model.add(LeakyReLU(0.1))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='softmax'))
+    
     return model
-
-if __name__ == '__main__':
-    model = create_model()
-    model.summary()
